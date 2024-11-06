a094b90dc037c2f55072a4ba909ca9f8971bbf75
Ran Ran <112669569+Ran-x2@users.noreply.github.com>
2024-11-06 16:47:20 -0500

    Update GSE178318.R
    Minimal formatting

--- GSE178318.R
+++ GSE178318.R
@@ -1,14 +1,18 @@
 library(Seurat)
-data <- Read10X(data.dir = 'F:/CRC/AA_DONE/GSE178318/')
+data <- Read10X(data.dir = 'GSE178318')
 seurat_object <- CreateSeuratObject(counts = data)
 saveRDS(seurat_object,'F:/CRC/GSE178318/srat.rds')
-# Data integrated, calling it integrated data from now on
+
+# Data already integrated, calling it integrated data from now on
 integrated_data = readRDS('F:/CRC/AA_Done/GSE178318/srat.rds')
+
 #original author encode the condition in the cell name, so we need to retrieve it
 obs_names = colnames(integrated_data)
+
 barcodes = list()
 patient =list()
 tissue = list()
+
 # Initialize vectors
 barcodes <- vector("character", length(obs_names))
 patient <- vector("character", length(obs_names))
@@ -38,4 +42,4 @@ mito.genes <- grep("^MT-", rownames(integrated_data), value = TRUE)
 # Calculate the percentage of mitochondrial gene expression per cell
 integrated_data[["percent.mt"]] <- PercentageFeatureSet(integrated_data, pattern = "^MT-")
 integrated_data <- subset(integrated_data, subset = nFeature_RNA > 200 & nFeature_RNA < 5000 & nCount_RNA > 500 & percent.mt < 20)
-saveRDS(integrated_data,'F:/CRC/AA_Done/GSE178318/processed_GSE178318.rds')
+saveRDS(integrated_data,'processed_GSE178318.rds')
